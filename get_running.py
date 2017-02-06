210a210277c39cce39c676daa54a60770d30808f
Frank <shufanhao@126.com>
2017-02-06 15:41:37 +0800

    Create get_running.py

---
+++ get_running.py
@@ -0,0 +1,42 @@
+
+HOST_IP = '172.16.172.128'
+PORT_BASE = 2000
+N = 42
+
+PASSWORD = "cisco"
+
+import pexpect
+
+
+def get_running(n):    
+    t = pexpect.spawn('telnet ' + HOST_IP + ' ' + str(PORT_BASE + i))
+
+    t.sendline("\r\n\r\n\r\n")
+    t.expect(">")
+    t.sendline('enable')
+    t.expect("Password:")
+    t.sendline(PASSWORD)
+    t.expect("#")
+    #m =t.expect([">", "#"])
+    #if m == 0:
+    #    t.sendline('enable')
+    #    t.expect("Password:")
+    #    t.sendline(PASSWORD)
+    t.sendline('term len 0')
+    t.expect("#")
+    t.sendline("show run")
+    t.expect("#")
+    data = t.before
+    t.sendline("exit")
+    print data
+
+    hostname = data.split('\n')[-1]
+
+    f = open(hostname + '.txt', "w")
+    f.write(data)
+    f.close()
+
+#for i in range(1, N+1):
+for i in range(31, N+1):
+    get_running(i)
+
